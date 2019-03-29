103a2cc613a2295e86a1fc2c762c9441e91550b9
HenryAvery <HenryAveryDev@gmail.com>
2019-03-29 08:05:52 -0500

    add port

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ const db = knex({
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {res.send('it is working');})
+app.get('/', (req, res) => {res.send('it is working')})
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', register.handleRegister(db, bcrypt))
 app.get('/profile/:id', profile.handleProfileGet(db, bcrypt))
 app.put('/image', image.handleImage(db))
 app.post('/imageurl', (req, res) =>{image.handleApiCall(req, res)})
-app.listen(3001, ()=> {
+app.listen(process.env.PORT, ()=> {
     console.log("app is running on port 3001");
 })
